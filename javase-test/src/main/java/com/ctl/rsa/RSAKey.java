920464b15a0f9480be7da74566937e882804f5ab
ctllin <guolinit@163.com>
2019-04-30 14:30:22 +0800

    'modify'

---
+++ javase-test/src/main/java/com/ctl/rsa/RSAKey.java
@@ -0,0 +1,68 @@
+package com.ctl.rsa;
+
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+/**
+ * <p>Title: RSAKeyStr</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <p>Company: www.hanshow.com</p>
+ * @author guolin
+ * @version 1.0
+ * @date 2019-04-30 13:19
+ */
+//@JsonIgnoreProperties({"publicKey", "privateKey"})
+public class RSAKey implements Serializable {
+    /**
+     * 私钥
+     */
+    private String privateKeyString;
+    /**
+     * 私钥
+     */
+    private RSAPrivateKey privateKey;
+    /**
+     * 公钥
+     */
+    private String publicKeyString;
+    /**
+     * 公钥
+     */
+    private RSAPublicKey publicKey;
+
+    public String getPrivateKeyString() {
+        return privateKeyString;
+    }
+
+    public void setPrivateKeyString(String privateKeyString) {
+        this.privateKeyString = privateKeyString;
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String getPublicKeyString() {
+        return publicKeyString;
+    }
+
+    public void setPublicKeyString(String publicKeyString) {
+        this.publicKeyString = publicKeyString;
+    }
+
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+}
