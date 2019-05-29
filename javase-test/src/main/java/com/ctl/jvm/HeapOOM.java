1f5bb5128bb64473a63cc18ea2641c4019051354
ctllin <guolinit@163.com>
2019-05-29 11:59:53 +0800

    'modify'

--- javase-test/src/main/java/com/ctl/jvm/HeapOOM.java
+++ javase-test/src/main/java/com/ctl/jvm/HeapOOM.java
@@ -20,11 +20,11 @@ public class HeapOOM {
     //-Xms2M -Xmx2M -Xss128k -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=E:\tmp -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime
     //生成的hprof可以使用Java VisualVM打开
     public static void main(String[] args) {
-//        List<OOMObject> list = new ArrayList<>();
-//        while (true) {
-//            list.add(new OOMObject());
-//        }
-        say();
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+        //say();
     }
     static void say(){
         say();
