62955898ad0526dd64d28c6fab3afdf9f0f26fa6
guolin <guolinit@163.com>
2019-08-21 17:15:36 +0800

    modify

--- spring-boot-kafka/src/main/java/com/dev/config/KafkaReceiver.java
+++ spring-boot-kafka/src/main/java/com/dev/config/KafkaReceiver.java
@@ -2,7 +2,9 @@ package com.dev.config;
 
 import com.alibaba.fastjson.JSON;
 import com.dev.bean.Message;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -13,6 +15,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -22,6 +25,7 @@ import java.util.*;
  * <p>Description: </p>
  * <p>Copyright: Copyright (c) 2018</p>
  * <p>Company: www.hanshow.com</p>
+ * Spring-Kafka中消息监听大致分为两种类型，一种是单条数据消费，一种是批量消费；
  *
  * @author guolin
  * @version 1.0
@@ -32,6 +36,7 @@ public class KafkaReceiver {
     private static Logger log = LoggerFactory.getLogger(KafkaReceiver.class);
     @Autowired
     private Environment environment;
+    final static Properties props = new Properties();
 
     private Map<String, Object> consumerProps() {
         Map<String, Object> props = new HashMap<>();
@@ -40,7 +45,7 @@ public class KafkaReceiver {
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 1000);
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);
         //一次拉取消息数量
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10); //对应batchListener(List<String> messages) 一次最多可以拉取10条数据
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 4); //对应batchListener(List<String> messages) 一次最多可以拉取4条数据
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
@@ -60,8 +65,11 @@ public class KafkaReceiver {
     }
 
 
-    final static Properties props = new Properties();
-
+    /**
+     * batchListener  and batchListener1 use one groupId
+     *
+     * @param messages
+     */
     @KafkaListener(id = "batchListener",
             topics = {TopicConst.EXECUTOR_TOPIC},
             containerFactory = "batchContainerFactory",
@@ -81,4 +89,27 @@ public class KafkaReceiver {
         }
     }
 
+    /**
+     * batchListener  and batchListener1 use one groupId
+     *
+     * @param messages
+     */
+    @KafkaListener(id = "batchListener1",
+            topics = {TopicConst.EXECUTOR_TOPIC},
+            containerFactory = "batchContainerFactory",
+            groupId = TopicConst.EXECUTOR_GROUPID)
+    public void batchListener1(List<String> messages) { //MAX_POLL_RECORDS_CONFIG
+        long timenow = System.currentTimeMillis();
+        try {
+            Thread.sleep(new Random().nextInt(100) + 50);
+        } catch (InterruptedException e) {
+        }
+        if (messages != null && messages.size() != 0) {
+            int size = messages.size();
+            messages.parallelStream().forEach(message -> {
+                Message msg = JSON.parseObject(message, Message.class);
+                log.info("Receiver--->batchListener1 size={}, {} 接收消息 message ={}", size, timenow, JSON.toJSONString(msg));
+            });
+        }
+    }
 }
