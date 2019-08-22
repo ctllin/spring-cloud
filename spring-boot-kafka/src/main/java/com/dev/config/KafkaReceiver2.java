4fd17146682e6871f1d9f0a56fdc26ad8bc3c05d
guolin <guolinit@163.com>
2019-08-22 14:20:15 +0800

    modify

--- spring-boot-kafka/src/main/java/com/dev/config/KafkaReceiver2.java
+++ spring-boot-kafka/src/main/java/com/dev/config/KafkaReceiver2.java
@@ -44,8 +44,8 @@ public class KafkaReceiver2 {
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         //一次拉取消息数量
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 4); //对应batchListener(List<String> messages) 一次最多可以拉取4条数据
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+//        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+//        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
@@ -56,14 +56,15 @@ public class KafkaReceiver2 {
     public ConcurrentKafkaListenerContainerFactory listenerContainer() {
         ConcurrentKafkaListenerContainerFactory factory  = new ConcurrentKafkaListenerContainerFactory();
         factory.setConsumerFactory(new DefaultKafkaConsumerFactory(consumerProps2()));
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
-
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         //设置并发量，小于或等于Topic的分区数
         //ConsumerConfig会初始化5次 onPool-worker-3,onPool-worker-2,onPool-worker-1,hListener-0-C-1,对应batchListener(List<String> messages)
         //最多会有四个线程取10条数据
         factory .setConcurrency(4);
         //设置为批量监听
         factory .setBatchListener(true);
+        factory.getContainerProperties().setPollTimeout(3000);
+
         return factory;
     }
 
